2de10d456617c94035af098d3dfce9de4dfdf223
ralbee1 <93877353+ralbee1@users.noreply.github.com>
2023-05-20 12:00:08 -0500

    Overhaul SecretManager get and create secret functions

--- legopython/lp_secretsmanager.py
+++ legopython/lp_secretsmanager.py
@@ -1,30 +1,30 @@
-#pylint: disable=line-too-long
-#This code was taken from "Sample Code" in secrets manager; Some information used from https://github.com/awslabs/aws-data-wrangler/blob/main/awswrangler/secretsmanager.py
-import json
+#pylint: disable=line-too-long, invalid-name
+#This code was revamped from the "Sample Code" in secrets manager; Some information used from https://github.com/awslabs/aws-data-wrangler/blob/main/awswrangler/secretsmanager.py
 import base64
-from typing import Any, Dict, cast, Union
 import boto3
 from botocore.exceptions import ClientError
 
-def get_secret(secret_name: str, region_name = 'us-east-1') -> str:
+
+def get_secret(secret_name: str, region_name = 'us-east-2') -> dict:
     '''
-    Return the raw text of a secret from secrets manager (NOTE -> you probably want to use _v2 and not this)
-    secret_name - The name of the secret to retrieve
+    Return the a decrypted, current version, secret from secrets manager as dict
+    secret_name - The name of the secret to retrieve  or the the Amazon Resource Name (ARN)
     region_name (opt) - The region the secret is stored in
-    Returns: str
+    Returns: Returns {'secret': secret} if rawtext or dict of key-value entries
     '''
-    # Create a Secrets Manager client
+    # Create a Secrets Manager client session #TODO: Find more efficient way to load session without duplicating code?
     session = boto3.session.Session()
     client = session.client(
         service_name = 'secretsmanager',
         region_name = region_name
     )
 
+    #https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/secretsmanager/client/get_secret_value.html#
     try:
         get_secret_value_response = client.get_secret_value(
             SecretId=secret_name
         )
-    except ClientError as e:
+    except ClientError as e: #Exception list: https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
         if e.response['Error']['Code'] == 'DecryptionFailureException':
             # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
             raise e
@@ -41,20 +41,47 @@ def get_secret(secret_name: str, region_name = 'us-east-1') -> str:
             # We can't find the resource that you asked for.
             raise e
     else:
-        # Decrypts secret using the associated KMS CMK. # Depending on whether the secret is a string or binary, one of these fields will be populated.
+        # Returns secret whether its a string or binary, 'SecretString' or 'SecretBinary' is populated
         if 'SecretString' in get_secret_value_response:
-            return get_secret_value_response['SecretString']
-        return base64.b64decode(get_secret_value_response['SecretBinary'])
+            if isinstance(get_secret_value_response['SecretString'], dict):
+                return get_secret_value_response['SecretString']
+            elif isinstance(get_secret_value_response['SecretString'], str): #Some secrets are stored only as plain text and not JSON, so return a dict with a key of 'secret'
+                return {'secret': get_secret_value_response['SecretString']} #secret
+            else:
+                raise ValueError('Unable to parse SecretString, did not recieve str or dict')
+            
+        elif 'SecretBinary' in get_secret_value_response:
+            return {'secret':base64.b64decode(get_secret_value_response['SecretBinary'])}
+        else:
+            raise ValueError('A SecretString or SecretBinary was not recieved from from request.')
 
 
-def get_secret_v2(secret_name: str, region_name = 'us-east-1') -> Union[Dict,str]:
+def create_secret(secret_name:str, secret_value:str, description:str = '', search_tags:list = None, force_overwrite:bool = False, region_name:str = 'us-east-2') -> dict:
     '''
-    Returns a dictionary of key/values from Secrets Manager
-    secret_name - The name of the secret to retrieve
-    region_name (opt) - The region the secret is stored in
-    Returns: Dict[str, Any] or Str
+    Creates a secretManager secretString, does not support SecretBinary
+    #TODO Allow SecretBinary support, can also support versioning, and replication
+
+    secret_name = accepts alphanumeric string and /_+=.@-
+    secret_value = accepts plain string or string formatted json: '{"key1":"value1, "key2":"value2"}'
+    description = Readable text explaining the use of the secret
+    search_tags = searchable tags, Each tag is a key and value pair of strings in a JSON text string: [{"Key":"CostCenter","Value":"12345"},{"Key":"environment","Value":"production"}]
+    force_overwrite = Boolean, if true will overwrite an identically named secret
+    region_name = Specifies aws instance to create the secret
+
+    Returns a http Json response of the result
+
+    Reference Docs: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/secretsmanager/client/create_secret.html
     '''
-    try:
-        return cast(Dict[str, Any], json.loads(get_secret(secret_name, region_name)))
-    except: #Some secrets are stored only as plain text and not JSON, so return a plain string if that's the case.
-        return get_secret(secret_name, region_name)
+    session = boto3.session.Session()
+    client = session.client(
+        service_name = 'secretsmanager',
+        region_name = region_name
+    )
+
+    if search_tags:
+        return client.create_secret(Name = secret_name, Description = description, SecretString = secret_value, Tags=search_tags,ForceOverwriteReplicaSecret = force_overwrite)
+    else:
+        return client.create_secret(Name = secret_name, Description = description, SecretString = secret_value, ForceOverwriteReplicaSecret = force_overwrite)
+
+result = create_secret(secret_name = 'create_dict_secret4', secret_value = '{"secretKey":"SecretValue"}', search_tags = None)
+print(result)
